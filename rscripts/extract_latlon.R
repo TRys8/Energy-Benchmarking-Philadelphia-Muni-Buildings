de40ca403c98bf3bb06aade35e280d600e0dde47
Gregory Park <gregoryjpark@gmail.com>
2015-10-28 11:17:54 -0400

    create small location data file (muni_locations.csv), add dirs for organization

---
+++ rscripts/extract_latlon.R
@@ -0,0 +1,22 @@
+library(stringr)
+
+energy_df <- read.csv("../2011-13 Philadelphia Municipal Building Energy Usage.csv", stringsAsFactors=F)
+
+loc_df <- energy_df[, c("Property.Id", "Property.Name", "Location")]
+
+loc_df$coords <- str_sub(loc_df$Location, start=sapply(loc_df$Location, FUN=function(x){str_locate(x, pattern=fixed("("))[1]}))
+
+loc_df$lat <- str_sub(loc_df$coords, 
+                      start=sapply(loc_df$coords, FUN=function(x){str_locate(x, pattern=fixed("("))[1] + 1}),
+                      end=sapply(loc_df$coords, FUN=function(x){str_locate(x, pattern=fixed(","))[1]} - 1)
+                      )
+
+loc_df$lon <- str_sub(loc_df$coords, 
+                      start=sapply(loc_df$coords, FUN=function(x){str_locate(x, pattern=fixed(","))[1] + 2}),
+                      end=sapply(loc_df$coords, FUN=function(x){str_locate(x, pattern=fixed(")"))[1]} - 1)
+                      )
+
+loc_df$Location <- NULL
+loc_df$coords <- NULL
+
+write.table(loc_df, "muni_locations.csv", sep=",", row.names=F)
