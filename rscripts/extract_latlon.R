0464a8c970b6c3c08da8397a321d5e86c71b2fcb
Gregory Park <gregoryjpark@gmail.com>
2015-10-28 11:35:40 -0400

    remove duplicates from location data

--- rscripts/extract_latlon.R
+++ rscripts/extract_latlon.R
@@ -1,8 +1,11 @@
 library(stringr)
+#setwd("~/Dropbox/Energy-Benchmarking-Philadelphia-Muni-Buildings/data")
 
 energy_df <- read.csv("../2011-13 Philadelphia Municipal Building Energy Usage.csv", stringsAsFactors=F)
 
 loc_df <- energy_df[, c("Property.Id", "Property.Name", "Location")]
+
+loc_df <- loc_df[!duplicated(loc_df$Property.Id),]
 
 loc_df$coords <- str_sub(loc_df$Location, start=sapply(loc_df$Location, FUN=function(x){str_locate(x, pattern=fixed("("))[1]}))
 
